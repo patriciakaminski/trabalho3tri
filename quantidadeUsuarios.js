60b0ae952a4b602583f71cfb43daceda755ad03c
patriciakaminski <168190743+patriciakaminski@users.noreply.github.com>
2024-10-19 20:02:12 -0300

    Update quantidadeUsuarios.js

--- quantidadeUsuarios.js
+++ quantidadeUsuarios.js
@@ -1,12 +1,10 @@
 import { getCSS, tickConfig } from "./common.js"
-
 async function quantidadeUsuariosPorRede() {
     const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
     const res = await fetch(url)
     const dados = await res.json()
     const nomeDasRedes = Object.keys(dados)
     const quantidadeDeUsuarios = Object.values(dados)
-
     const data = [
         {
             x: nomeDasRedes, 
@@ -49,11 +47,9 @@ async function quantidadeUsuariosPorRede() {
             }
         }
     }
-
     const grafico = document.createElement('div')
     grafico.className = 'grafico'
     document.getElementById('graficos-container').appendChild(grafico)
     Plotly.newPlot(grafico, data, laytout)
-}
-
+    }
 quantidadeUsuariosPorRede()
